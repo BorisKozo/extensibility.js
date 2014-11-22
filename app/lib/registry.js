7cb4bdda70bf430fb9be26a9653766b75d87779b
Boris Kozorovitzky <majorzbzzn@gmail.com>
2014-11-22 23:22:40 +0200

    basic registry logic WIP

--- app/lib/registry.js
+++ app/lib/registry.js
@@ -1,18 +1,49 @@
 ﻿(function (EJS) {
   'use strict';
-  var _registry = {};
+  var _registry;
   var _delimiter = '/'; //The delimiter that will be used for all the path construction, path axes may not contain this delimiter
-  var userPathPrefix = 'user';
 
+  function Node(name) {
+    this.name = name;
+    this.nodes = {};
+    this.addins = [];
+  }
+
+  Node.prototype.addAddin = function (addin) {
+    this.addins.push(addin);
+  };
+
+  function getNode(axes, createIfNotExists) {
+    if (_.isString(axes)) {
+      axes = EJS.registry.breakPath(axes);
+    }
+    var currentNode = _registry;
+    var i;
+    for (i = 0; i < axes.length; i++) {
+      if (!currentNode.nodes[axes[i]]) {
+        if (createIfNotExists) {
+          currentNode.nodes[axes[i]] = new Node(axes[i]);
+        } else {
+          return null;
+        }
+      }
+      currentNode = currentNode.nodes[axes[i]];
+    }
+
+    return currentNode;
+  }
+
+  
 
   EJS.registry = {
+    systemPathPrefix: 'EJS',
 
     /***
      * Returns true if the axis is valid for the given delimiter
      */
     verifyAxis: function (axis, delimiter) {
       if (_.isString(axis)) {
-        if (_.isEmpty(axis) || _.indexOf(axis, _delimiter) >= 0) {
+        if (_.isEmpty(axis) || _.indexOf(axis, delimiter) >= 0) {
           return false;
         }
 
@@ -20,25 +51,85 @@
       }
       return false;
     },
+    /***
+    * Builds a path from the given axes, if one of the axes is invalid an exception will be thrown
+    */
     buildPath: function () {
       if (arguments.length === 0) {
-        return _delimiter;
+        return '';
       }
       if (arguments[0] instanceof Array) {
-        return EJS.registry.buildPath.apply(this, _.flatten(arguments[0]))
+        return EJS.registry.buildPath.apply(this, _.flatten(arguments[0]));
       }
       var args = Array.prototype.slice.call(arguments, 0);
       return args.reduce(function (previous, current) {
         if (EJS.registry.verifyAxis(current, _delimiter)) {
+          if (previous === '') {
+            return current;
+          }
           return previous + _delimiter + current;
         } else {
           throw new Error('Illegal path axis ' + current + ' for delimiter ' + _delimiter);
         }
       }, '');
     },
-    set: function () {
-
+    /***
+    * Breaks the given path to its axes, if the path is invalid an exception is thrown
+    */
+    breakPath: function (path) {
+      if (!_.isString(path)) {
+        throw new Error('path must be a string ' + JSON.stringify(path));
+      }
+      if (path === '') {
+        return [];
+      }
+      var splitPath = path.split(_delimiter);
+      _.forEach(splitPath, function (axis) {
+        if (!EJS.registry.verifyAxis(axis, _delimiter)) {
+          throw new Error('Invalid axis ' + axis);
+        }
+      });
+      return splitPath;
+    },
+    /**
+    * returns true if the given path exists in the registry and false otherwise
+    */
+    nodeExists: function (path) {
+      var node = getNode(path, false);
+      return node !== null;
+    },
+    /**
+    * Adds the given addin to the node at the given path
+    * If no addin is specifies creates an empty node at the path
+    */
+    addAddin: function (path, addin) {
+      var node = getNode(path, true);
+      if (addin) {
+        node.addAddin(addin);
+      }
+    },
+    /**
+    * Returns an array of all the addins immediately under the given path using the search criteria
+    * Search criteria goes as the parameter for lodash filter function, if undefined then all the addins are returned
+    * Returns null if the path doesn't exist
+    */
+    getAddins: function (path, searchCriteria) {
+      var node = getNode(path, false);
+      if (node === null) {
+        return null;
+      }
+      if (searchCriteria === undefined) {
+        return _.clone(node.addins);
+      }
+      return _.filter(node.addins, searchCriteria);
+    },
+    /**
+    * Clears the registry of all the nodes (you shouldn't use this function)
+    */
+    clear: function () {
+      _registry = new Node('');
     }
-  }
+  };
 
-})(window.EJS || (window.EJS = {}))
+  EJS.registry.clear();
+})(window.EJS || (window.EJS = {}));
--- test/unit/registry.spec.js
+++ test/unit/registry.spec.js
@@ -1,7 +1,11 @@
 ﻿describe('Registry', function () {
   'use strict';
   var expect = chai.expect;
+  var EJS = window.EJS;
 
+  afterEach(function () {
+    EJS.registry.clear();
+  });
 
   describe('Verify Axis', function () {
 
@@ -26,21 +30,121 @@
       expect(EJS.registry.verifyAxis('Hello world', '/')).to.be.true;
     });
 
-  })
+  });
 
   describe('Build Path', function () {
     it('should build an empty path when called with no args', function () {
-      expect(EJS.registry.buildPath()).to.be.equal('/');
+      expect(EJS.registry.buildPath()).to.be.equal('');
     });
 
     it('should build a path from array', function () {
-      expect(EJS.registry.buildPath(['a', 'bcd', 'ef'])).to.be.equal('/a/bcd/ef');
-      expect(EJS.registry.buildPath(['a', ['bcd'], ['ef','g']])).to.be.equal('/a/bcd/ef/g');
+      expect(EJS.registry.buildPath(['a', 'bcd', 'ef'])).to.be.equal('a/bcd/ef');
+      expect(EJS.registry.buildPath(['a', ['bcd'], ['ef', 'g']])).to.be.equal('a/bcd/ef/g');
     });
 
     it('should build a path from arguments', function () {
-      expect(EJS.registry.buildPath('a', 'bcd', 'ef')).to.be.equal('/a/bcd/ef');
+      expect(EJS.registry.buildPath('a')).to.be.equal('a');
+      expect(EJS.registry.buildPath('a', 'bcd', 'ef')).to.be.equal('a/bcd/ef');
+    });
+
+    it('should throw if one of the axes contains the delimiter', function () {
+      expect(function () {
+        EJS.registry.buildPath('a', 'b/cd', 'ef');
+      }).to.throw('Illegal path axis b/cd for delimiter /');
     });
   });
+
+  describe('Break Path', function () {
+    it('should throw if the path is not a string', function () {
+      expect(function () {
+        EJS.registry.breakPath({});
+      }).to.throw('path must be a string {}');
+
+    });
+
+    it('should throw if the path contains empty axes', function () {
+      expect(function () {
+        EJS.registry.breakPath('/a/b/c');
+      }).to.throw('Invalid axis ');
+      expect(function () {
+        EJS.registry.breakPath('a/b/c/');
+      }).to.throw('Invalid axis ');
+
+    });
+
+    it('should break an empty path', function () {
+      expect(EJS.registry.breakPath('')).to.be.eql([]);
+    });
+
+    it('should break a path', function () {
+      expect(EJS.registry.breakPath('abv')).to.be.eql(['abv']);
+      expect(EJS.registry.breakPath('abv/efg/aaa')).to.be.eql(['abv', 'efg', 'aaa']);
+    });
+  });
+
+  describe('Node Exists', function () {
+    it('should return false for non existing nodes', function () {
+      expect(EJS.registry.nodeExists('abv')).to.be.false;
+      expect(EJS.registry.nodeExists('abv/aaa')).to.be.false;
+    });
+
+    it('should return true for existing nodes', function () {
+      EJS.registry.addAddin('abv');
+      expect(EJS.registry.nodeExists('abv')).to.be.true;
+
+      EJS.registry.addAddin('abv/ccc/a');
+      expect(EJS.registry.nodeExists('abv/ccc/a')).to.be.true;
+      expect(EJS.registry.nodeExists('abv/ccc')).to.be.true;
+
+    });
+  });
+
+  describe('Add Addin', function () {
+    it('should add an addin to a specific node', function () {
+      var addin = {};
+      EJS.registry.addAddin('a/b/c', addin);
+      expect(EJS.registry.getAddins('a/b/c')[0]).to.be.equal(addin);
+    });
+
+    it('should create a node if the addin is undefined', function () {
+      
+      EJS.registry.addAddin('a/b/c');
+      expect(EJS.registry.nodeExists('a/b/c')).to.be.true;
+    });
+  });
+
+  describe('Get Addins', function () {
+    it('should not get addins of non existing node', function () {
+      expect(EJS.registry.getAddins('abv/ccc/a')).to.be.null;
+    });
+
+    it('should get all addins of a node', function () {
+      var addin1 = {};
+      var addin2 = {};
+      EJS.registry.addAddin('ab/cd', addin1);
+      EJS.registry.addAddin('ab/cd', addin2);
+      expect(EJS.registry.getAddins('ab/cd')).to.be.eql([addin1, addin2]);
+    });
+
+    it('should get all addins of a node with a specific property', function () {
+      var addin1 = {id:1};
+      var addin2 = {id:2};
+      EJS.registry.addAddin('ab/cd', addin1);
+      EJS.registry.addAddin('ab/cd', addin2);
+      expect(EJS.registry.getAddins('ab/cd', { id: 1 })).to.be.eql([addin1]);
+    });
+
+  });
+
+  describe('Clear', function () {
+    it('should clear the registry', function () {
+      expect(EJS.registry.nodeExists('aaa')).to.be.false;
+      EJS.registry.addAddin('aaa');
+      expect(EJS.registry.nodeExists('aaa')).to.be.true;
+      EJS.registry.clear();
+      expect(EJS.registry.nodeExists('aaa')).to.be.false;
+    });
+  });
+
 
 });
